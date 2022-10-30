82b536517ad4c5a066a5901ba3773180dca0af20
Kevin T <30321316+glaciyan@users.noreply.github.com>
2022-10-30 18:28:04 +0100

    outline search bar on focus

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -11,7 +11,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="bg-slate-100 py-2 px-4 rounded shadow my-4">
+  <div class="bg-slate-100 py-2 px-4 rounded shadow my-4 focus-within:ring-2 focus-within:ring-black transition-shadow">
     <input
       ref="input"
       class="w-full bg-transparent focus:outline-none"
