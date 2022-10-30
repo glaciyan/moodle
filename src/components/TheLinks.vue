34245c437897df5f28940b1477fb84fe90f38dc9
Kevin T <30321316+glaciyan@users.noreply.github.com>
2022-10-30 21:25:32 +0100

    reactor theme button into component

--- src/components/TheLinks.vue
+++ src/components/TheLinks.vue
@@ -1,40 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
-
-const theme = ref<string | null>();
-
-onMounted(() => {
-  theme.value = localStorage.getItem("theme");
-});
-
-const switchTheme = () => {
-  switch (theme.value) {
-    case null:
-      theme.value = "dark";
-      localStorage.setItem("theme", "dark");
-      document.documentElement.classList.add("dark");
-      break;
-
-    case "dark":
-      theme.value = "light";
-      localStorage.setItem("theme", "light");
-      document.documentElement.classList.remove("dark");
-      break;
-
-    case "light":
-      theme.value = null;
-      localStorage.removeItem("theme");
-      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        document.documentElement.classList.add("dark");
-      } else {
-        document.documentElement.classList.remove("dark");
-      }
-      break;
-
-    default:
-      break;
-  }
-};
+import ThemeButton from "./ThemeButton.vue"
 </script>
 
 <template>
@@ -51,11 +16,6 @@ const switchTheme = () => {
     >
       glaciyan.cc
     </a>
-    <a
-      class="text-blue-400 hover:underline focus:underline cursor-pointer"
-      @click="switchTheme"
-    >
-      {{ theme === null ? "os theme" : theme === "dark" ? "dark" : "light" }}
-    </a>
+    <ThemeButton />
   </div>
 </template>
---
+++ src/components/ThemeButton.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const theme = ref<string | null>();
+
+onMounted(() => {
+  theme.value = localStorage.getItem("theme");
+});
+
+const switchTheme = () => {
+  switch (theme.value) {
+    case null:
+      theme.value = "dark";
+      localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
+      break;
+
+    case "dark":
+      theme.value = "light";
+      localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
+      break;
+
+    case "light":
+      theme.value = null;
+      localStorage.removeItem("theme");
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+      }
+      break;
+
+    default:
+      break;
+  }
+};
+</script>
+
+<template>
+  <a
+    class="text-blue-400 hover:underline focus:underline cursor-pointer"
+    @click="switchTheme"
+  >
+    {{ theme === null ? "os theme" : theme === "dark" ? "dark" : "light" }}
+  </a>
+</template>
