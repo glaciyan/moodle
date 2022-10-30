fae40ddaecb4c4cdde83090da673c0f4cc092f72
Kevin T <30321316+glaciyan@users.noreply.github.com>
2022-10-30 19:52:42 +0100

    min h screen height

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ const { allCourses, isLoading } = useFetchCourses();
 </script>
 
 <template>
-  <div class="max-w-md md:mx-auto mx-2">
+  <div class="max-w-md md:mx-auto mx-2 min-h-screen">
     <div class="md:my-12 my-4">
       <h1 class="md:text-2xl text-xl">Moodle Kurse</h1>
       <TheLinks />
