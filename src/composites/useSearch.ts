bff59fd4239fdc9749b141c83d4df9dc334446f3
Kevin T <30321316+glaciyan@users.noreply.github.com>
2022-11-02 20:33:28 +0100

    provide fuseResults from search

--- src/composites/useSearch.ts
+++ src/composites/useSearch.ts
@@ -1,6 +1,6 @@
-import type Fuse from "fuse.js";
-import { computed, ref, watch } from "vue";
 import { throttledWatch } from "@vueuse/core";
+import type Fuse from "fuse.js";
+import { computed, ref } from "vue";
 import type { Course } from "../types/Course";
 
 export function useSearch(data: Course[]) {
@@ -12,30 +12,38 @@ export function useSearch(data: Course[]) {
   const courses = ref(data);
   const noResults = computed(() => courses.value.length === 0);
 
-  throttledWatch(search, async (value) => {
-    if (value.length === 0) {
-      courses.value = data;
-      return;
+  const fuseResults = ref<Fuse.FuseResult<Course>[]>();
+
+  throttledWatch(
+    search,
+    async (value) => {
+      if (value.length === 0) {
+        courses.value = data;
+        return;
+      }
+
+      if (!fuse) {
+        const Fuse = (await import("fuse.js")).default;
+        fuse = new Fuse(data, {
+          keys: ["name"],
+          includeScore: true,
+        });
+      }
+
+      const result = fuse.search(value);
+      fuseResults.value = result;
+      console.log(result);
+
+      if (result.length === 0) {
+        courses.value = [];
+      } else {
+        courses.value = result.map((r) => r.item);
+      }
+    },
+    {
+      throttle: 300,
     }
+  );
 
-    if (!fuse) {
-      const Fuse = (await import("fuse.js")).default;
-      fuse = new Fuse(data, {
-        keys: ["name"]
-      });
-    }
-
-    const result = fuse.search(value);
-    console.log(result);
-
-    if (result.length === 0) {
-      courses.value = [];
-    } else {
-      courses.value = result.map((r) => r.item);
-    }
-  }, {
-      throttle: 300
-  });
-
-  return { search, emptySearch, courses, noResults };
+  return { search, emptySearch, courses, noResults, fuseResults };
 }
