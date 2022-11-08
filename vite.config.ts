c1d207e8613665329795d670fbf60d1815578b7d
Kevin T <30321316+glaciyan@users.noreply.github.com>
2022-11-08 15:06:41 +0100

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      injectRegister: "auto",
+    }),
+  ],
+  build: {
+    sourcemap: true,
+  },
 });
