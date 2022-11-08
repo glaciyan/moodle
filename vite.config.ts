47edb89fcfd02e1f3b70c9661173f6bc6fb4f18c
Kevin T <30321316+glaciyan@users.noreply.github.com>
2022-11-08 15:56:36 +0100

    pwa plugin settings

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,47 @@ export default defineConfig({
         enabled: true,
       },
       injectRegister: "auto",
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /https:\/\/moodle-worker-api\.glaciyan\.cc\/.*/i,
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "course-cache",
+            },
+          },
+        ],
+      },
+      manifest: {
+        name: "HTWG Moodle Shortcuts",
+        short_name: "Moodle",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/maskable_icon_192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/maskable_icon_512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        theme_color: "#4870fd",
+        background_color: "#4870fd",
+      },
     }),
   ],
   build: {
