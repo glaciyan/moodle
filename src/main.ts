06e3899c89608a6238bf133f8198aede3485442d
Kevin T <30321316+glaciyan@users.noreply.github.com>
2022-12-06 10:45:35 +0100

    dev market in title

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 
+if (import.meta.env.DEV) {
+  document.title = "(DEV) " + document.title;
+}
+
 createApp(App).mount("#app");
